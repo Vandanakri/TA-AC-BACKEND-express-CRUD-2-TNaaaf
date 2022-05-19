80094259e68c5eef98813d79bd14701c69820051
vandana <vandana123annusah@gmail.com>
2022-05-19 15:07:09 +0530

    BNaadn

--- block-BNaadn/book/app.js
+++ block-BNaadn/book/app.js
@@ -3,9 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var bookRouter = require('./routes/book');
+
+
+//connecting to database
+
+mongoose.connect("mongodb://localhost/blog",
+  { useNewUrlParser: true, useunifiedTopology: true },
+  (err) => {
+    console.log(err ? err : "connected true");
+  })
+
 
 var app = express();
 
@@ -20,15 +31,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/book', bookRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -39,3 +50,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
---
+++ block-BNaadn/book/models/Book.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var bookSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    summary: { type: String },
+    pages: Number,
+    publication: String,
+    category: [{ type: String }],
+    author: { type: String },
+    likes: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+var Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
